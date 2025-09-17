5b95f07e12b22592f80404e47207958fd3887614
LuisReis09 <luis.reis@academico.ufpb.br>
2025-09-17 14:11:07 -0300

    Refactor main function by removing redundant solution validation and improving solution iteration logic

--- cppver2/teste.cpp
+++ cppver2/teste.cpp
@@ -29,95 +29,18 @@ int main(){
         cout << "Solucao INVALIDA" << endl;
     }
 
+    int iter = 500;
+    while(iter--){
+        cout << "Iteracao " << (500 - iter) << endl;
+        MelhorarSolucao(s.rotas, s.custo_total);
+        s.custo_total = CustoTotal(s.rotas);
+        VNDIntraInter(s.rotas);
+        s.custo_total = CustoTotal(s.rotas);
+    }
+
+    cout << "====== Solucao Final ======" << endl;
+    s.SalvarSolucao("solucao_teste_final.sol");
     
     
-    s.SalvarSolucao("solucao_so_guloso.txt");
-
-    cout << "======= Melhora Solucao =======" << endl;
-    MelhorarSolucao(s.rotas, s.custo_total);
-    s.custo_total = CustoTotal(s.rotas);
-    if(VerificaSolucao(s.rotas, true)){
-        cout << "Solucao VALIDA" << endl;
-        cout << "Custo: " << s.custo_total << endl;
-        cout << "Veiculos usados: " << s.rotas.size() << endl;
-        for(const vector<int>& r : s.rotas){
-            for(const int& e : r){
-                cout << e << " ";
-            }
-            cout << endl;
-        }
-    }else{
-        cout << "Solucao INVALIDA" << endl;
-    }
-
-
-    cout << "====== VND ======" << endl;
-    VNDIntraInter(s.rotas);
-    s.custo_total = CustoTotal(s.rotas);
-
-    if(VerificaSolucao(s.rotas, true)){
-        cout << "Solucao VALIDA" << endl;
-        cout << "Custo: " << s.custo_total << endl;
-        cout << "Veiculos usados: " << s.rotas.size() << endl;
-        for(const vector<int>& r : s.rotas){
-            for(const int& e : r){
-                cout << e << " ";
-            }
-            cout << endl;
-        }
-    }else{
-        cout << "Solucao INVALIDA" << endl;
-    }
-
-    // for(int i = 0; i < 5; i++){
-    //     ThreadData *data = new ThreadData(i, s.rotas, 10000, 1000, s.custo_total, pool);
-        
-    //     cout << "Iniciando thread " << i << endl;
-    //     pool.EnqueueTask(ThreadILS, (void *) data);
-    //     cout << "Thread " << i << " iniciada." << endl;
-    // }
-
-    // pool.WaitAllThreads();
-
-    // cout << "====== ILS ======" << endl;
-    // ILS(s.rotas);
-    // s.custo_total = CustoTotal(s.rotas);
-
-    // if(VerificaSolucao(s.rotas, true)){
-    //     cout << "Solucao VALIDA" << endl;
-    //     cout << "Custo: " << s.custo_total << endl;
-    //     cout << "Veiculos usados: " << s.rotas.size() << endl;
-    //     for(const vector<int>& r : s.rotas){
-    //         for(const int& e : r){
-    //             cout << e << " ";
-    //         }
-    //         cout << endl;
-            
-    //     }
-    //     s.SalvarSolucao("solucao_teste_ils.txt");
-    // }else{
-    //     cout << "Solucao INVALIDA" << endl;
-    // }
-
-    cout << "====== VND ======" << endl;
-    VNDIntraInter(s.rotas);
-    s.custo_total = CustoTotal(s.rotas);
-
-    if(VerificaSolucao(s.rotas, true)){
-        cout << "Solucao VALIDA" << endl;
-        cout << "Custo: " << s.custo_total << endl;
-        cout << "Veiculos usados: " << s.rotas.size() << endl;
-        for(const vector<int>& r : s.rotas){
-            for(const int& e : r){
-                cout << e << " ";
-            }
-            cout << endl;
-        }
-        s.SalvarSolucao("solucao_teste.txt");
-    }else{
-        cout << "Solucao INVALIDA" << endl;
-    }
-
-
     return 0;
 }
